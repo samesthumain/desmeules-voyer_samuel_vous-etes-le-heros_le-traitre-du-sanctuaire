bfb5c2581acf34e2b315f6e71095b600f9344f8d
Samuel Desmeules-Voyer <samueldv47@gmail.com>
2023-09-27 17:05:13 -0400

    pas fini

---
+++ script.js
@@ -0,0 +1,37 @@
+let chapters = {
+    let debut = {
+        titre: "Arrivée au sanctuaire",
+        description: "Javier, chevalier de bronze au service du sanctuaire d'Athéna, a recu une lettre du sanctuaire lui informant qu'il y a un traitre chez les chevaliers. <br> Dès son arrivée au sanctuaire, Javier est attaqué par un autre chevalier. Il semble porter un bouclier, comment Javier compte-t-il riposter?",
+        image:"",
+        boutons: [,],
+    }
+
+    let erreur = {
+        titre: "Arrivée au sanctuaire",
+        description: "",
+        image:"",
+        boutons:[],
+    }
+    
+    let confiance = {
+        titre: "Rencontre agitée",
+        description: "Le bouclier de son ennemi ne peut rien conrte la rafale de coups de poing rapides de Javier, masi celui-ci ne peut pas se reposer pour autant: au loin, il vois un autre chevalier de bronze fuire un chevalier d'or, le plus haut rang possible pour un chevalier. <br> Le chevalier de bronze supplie Javier de l'aider puisque seul, il ne peut rien contre un chevalier de bronze, que fait Javier?",
+        image:"",
+        boutons: [, , ],
+    }
+
+    let erreur = {
+        titre: "Arrivée au sanctuaire",
+        description: "",
+        image:"",
+        boutons:[],
+    }
+
+    let traitre = {
+        titre: "Ensemble, Javier et l'autre chevalier de broze qui se présente sous le nom de Lancelot ont réussit à battre un chevalier d'or. Lancelot explique ensuite la raison pour laquelle le chevalier d'or le chassait: il a découvert l'identité du traitre original et que le chevalier d'or avait rejoint sa cause",
+        description: "",
+        image:"",
+        boutons:[],
+    }
+    
+}
