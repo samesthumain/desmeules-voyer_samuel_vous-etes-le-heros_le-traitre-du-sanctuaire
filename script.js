1118d69a5b97356bc5c070b7bc4dfb0ad5ff9989
Samuel Desmeules-Voyer <samueldv47@gmail.com>
2023-09-28 17:55:25 -0400

    big one

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 let chapters = {
-    let debut = {
+     debut: {
         titre: "Arrivée au sanctuaire",
-        description: "Javier, chevalier de bronze au service du sanctuaire d'Athéna, a recu une lettre du sanctuaire lui informant qu'il y a un traitre chez les chevaliers. <br> Dès son arrivée au sanctuaire, Javier est attaqué par un autre chevalier. Il semble porter un bouclier, comment Javier compte-t-il riposter?",
-        image:"",
+        description: "Javier, chevalier de bronze au service du sanctuaire d'Athéna, a recu une lettre provenant dudis sanctuaire lui informant qu'il y a un traitre chez les chevaliers. Il décide donc de rentrer pour offrir son aide.",
+        image:"assets",
         boutons: [,],
-    }
+    },
 
-    let erreur = {
-        titre: "Arrivée au sanctuaire",
-        description: "",
+     ambuscade: {
+        titre: "Attaque surprise",
+        description: "Dès son arrivée au sanctuaire, Javier est attaqué par un autre chevalier. Celui-ci semble porter un bouclier, comment Javier compte-t-il riposter?",
         image:"",
         boutons:[],
-    }
+    },
+
+    //première mort optionelle
+    bloque: {
+        titre: "Bloqué",
+        description: "Tout miser sur une seule attaque contre un adversaire avec une bonne défence semble avoir été une mauvaise idée, l'agresseur profite du manque de jugement de Javier et le tue d'un coup rapide et précis. «Sus à la fausse déesse!» fut la dernière chose qu'il entendit.",
+        image:"",
+        boutons:[],
+    },
+
+    rafale: {
+        titre: "Un traitre de moins",
+        description: "Le bouclier de son ennemi ne peut rien contre la rafale de coups de poing rapides. Le chevalier finit par tomber sous le poids des coups sans arrèt de Javier, marmonant «La Déesse tombera! Nous allons...» avant de tomber sans connaissance.",
+        image:"",
+        boutons:[],
+    },
+
     
-    let confiance = {
+     rencontre: {
         titre: "Rencontre agitée",
-        description: "Le bouclier de son ennemi ne peut rien conrte la rafale de coups de poing rapides de Javier, masi celui-ci ne peut pas se reposer pour autant: au loin, il vois un autre chevalier de bronze fuire un chevalier d'or, le plus haut rang possible pour un chevalier. <br> Le chevalier de bronze supplie Javier de l'aider puisque seul, il ne peut rien contre un chevalier de bronze, que fait Javier?",
+        description: "Alors qu'il continue d'avancer dans le sanctuaire, se demandant comment il pourrait trouver le reste du «Nous» qu'a mentioné le chevalier qu'il a battu, au loin, Javier vois un autre chevalier de bronze fuire un chevalier d'or, le plus haut rang possible pour un chevalier.",
         image:"",
         boutons: [, , ],
-    }
+    },
 
-    let erreur = {
+    rencontre: {
+        titre: "Rencontre agitée",
+        description: "Le chevalier de bronze supplie Javier de l'aider puisque seul, il ne peut rien contre un chevalier d'or, que fait Javier, devrait-il le croire?",
+        image:"",
+        boutons: [, , ],
+    },
+
+     erreur: {
         titre: "Arrivée au sanctuaire",
         description: "",
         image:"",
         boutons:[],
-    }
+    },
 
-    let traitre = {
+     traitre: {
         titre: "Ensemble, Javier et l'autre chevalier de broze qui se présente sous le nom de Lancelot ont réussit à battre un chevalier d'or. Lancelot explique ensuite la raison pour laquelle le chevalier d'or le chassait: il a découvert l'identité du traitre original et que le chevalier d'or avait rejoint sa cause",
         description: "",
         image:"",
         boutons:[],
-    }
+    },
     
 }
