32e9aa8c97899b967f304506491e121cd1b989fa
Samuel Desmeules-Voyer <samueldv47@gmail.com>
2023-09-28 18:48:34 -0400

    big two

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let chapters = {
-     debut: {
+    debut: {
         titre: "Arrivée au sanctuaire",
         description: "Javier, chevalier de bronze au service du sanctuaire d'Athéna, a recu une lettre provenant dudis sanctuaire lui informant qu'il y a un traitre chez les chevaliers. Il décide donc de rentrer pour offrir son aide.",
         image:"assets",
-        boutons: [,],
+        boutons: [],
     },
 
-     ambuscade: {
+    ambuscade: {
         titre: "Attaque surprise",
         description: "Dès son arrivée au sanctuaire, Javier est attaqué par un autre chevalier. Celui-ci semble porter un bouclier, comment Javier compte-t-il riposter?",
         image:"",
-        boutons:[],
+        boutons:[, ],
     },
 
-    //première mort optionelle
+    //mort 1
     bloque: {
         titre: "Bloqué",
         description: "Tout miser sur une seule attaque contre un adversaire avec une bonne défence semble avoir été une mauvaise idée, l'agresseur profite du manque de jugement de Javier et le tue d'un coup rapide et précis. «Sus à la fausse déesse!» fut la dernière chose qu'il entendit.",
@@ -22,36 +22,62 @@ let chapters = {
     },
 
     rafale: {
-        titre: "Un traitre de moins",
+        titre: "Un traitre en moins",
         description: "Le bouclier de son ennemi ne peut rien contre la rafale de coups de poing rapides. Le chevalier finit par tomber sous le poids des coups sans arrèt de Javier, marmonant «La Déesse tombera! Nous allons...» avant de tomber sans connaissance.",
         image:"",
         boutons:[],
     },
 
     
-     rencontre: {
+    rencontre: {
         titre: "Rencontre agitée",
         description: "Alors qu'il continue d'avancer dans le sanctuaire, se demandant comment il pourrait trouver le reste du «Nous» qu'a mentioné le chevalier qu'il a battu, au loin, Javier vois un autre chevalier de bronze fuire un chevalier d'or, le plus haut rang possible pour un chevalier.",
         image:"",
-        boutons: [, , ],
+        boutons: [, ],
     },
 
-    rencontre: {
-        titre: "Rencontre agitée",
+    dilemme: {
+        titre: "Le bronze et l'or",
         description: "Le chevalier de bronze supplie Javier de l'aider puisque seul, il ne peut rien contre un chevalier d'or, que fait Javier, devrait-il le croire?",
         image:"",
         boutons: [, , ],
     },
 
-     erreur: {
-        titre: "Arrivée au sanctuaire",
-        description: "",
+    //mort 2
+    attaquer: {
+        titre: "Rouler sous l'or",
+        description: "S'il y a une personne en qui on peut croire, c'est le chevalier d'or, le plus fort et le plus loyal des chevaliers! Javier donne un puissant coup de poing au crâne de l'autre chevalier de bronze, l'assomant instantanément. «Quel dommage, tu viens d'éliminer un des seuls aliés qui vous restait à vous, les loyaux» dit le chevalier d'or avans d'oblitérer le deux chevaliers de bronze devans lui",
         image:"",
         boutons:[],
     },
 
-     traitre: {
-        titre: "Ensemble, Javier et l'autre chevalier de broze qui se présente sous le nom de Lancelot ont réussit à battre un chevalier d'or. Lancelot explique ensuite la raison pour laquelle le chevalier d'or le chassait: il a découvert l'identité du traitre original et que le chevalier d'or avait rejoint sa cause",
+    //passage sans twist début
+    attendre: {
+        titre: "Réaction tardive",
+        description: "Ne voulant pas prendre de décision hâtive, Javier attends un peu avant d'attaquer sans raison. Une décision qu'il regrette amèrement lorsqu'il voit le chavalier d'or réduire celui qui le fuiait en poussières avant de se tourner vers Javier, un sourire moqueur sur le visage.",
+        image:"",
+        boutons:[],
+    },
+
+    riposte: {
+        titre: "combat perdu d'avance",
+        description: "«T'es un peu lent, toi.» Dis le chevalier d'or en s'aprochant. Javier n'as plus le temps d'hésiter le chevalier d'or n'est clairement pas de son côté mais que peut-il faire contre le plus puissant chevalier du sanctuaire?",
+        image:"",
+        boutons:[],
+    },
+
+    //mort optionelle
+    desespere: {
+        titre: "Même pas proche",
+        description: "Désespéré, Javier donne le plus de coups de poing possible, le plus vite possible mais aucun de ces petit coups rapides ne'est assez puissant pour battre un chevalier de bronze. Javier est oblitéré en un clin d'oeil",
+        image:"",
+        boutons:[],
+    },
+
+    //passage sans twist fin
+
+    traitre: {
+        titre: "Ensemble, Javier et l'autre chevalier de bronze qui se présente sous le nom de Lancelot ont réussit à battre un chevalier d'or. Lancelot explique ensuite la raison pour laquelle le chevalier d'or le chassait: il a découvert l'identité du traitre original et que le chevalier d'or avait rejoint sa cause",
         description: "",
         image:"",
         boutons:[],
