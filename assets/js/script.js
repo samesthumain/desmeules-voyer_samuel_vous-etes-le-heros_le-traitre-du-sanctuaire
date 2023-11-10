1b2a845124e9cb7991b1907ce95b52c9cb500cc6
Samuel Desmeules-Voyer <112189796+samesthumain@users.noreply.github.com>
2023-11-10 13:17:02 -0500

    fix sons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const chapters = {
         description: "Javier, membre de l'ordre des chevalier de bronze qui se battent au service du sanctuaire d'Athéna à l'aide de l'énergie divine de la Déesse, a recu une lettre provenant dudis sanctuaire lui informant qu'il y a un traitre chez les chevaliers et que son influence se répend à travers le sanctuaire. Il décide donc de rentrer pour offrir son aide.",
         image: "./assets/images/sanctuaire.jfif",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 494,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Allons-y!',
             destination: 'ambuscade'
@@ -15,6 +17,8 @@ const chapters = {
         description: "Dès son arrivée au sanctuaire, Javier est attaqué par un autre chevalier. Celui-ci semble porter un bouclier, comment Javier compte-t-il riposter?",
         image: "./assets/images/ambuscade.jpeg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 288,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Un coup puissant, direct',
             destination: 'bloque'
@@ -32,6 +36,8 @@ const chapters = {
         description: "Tout miser sur une seule attaque contre un adversaire avec une bonne défence semble avoir été une mauvaise idée, l'agresseur profite du manque de jugement de Javier et le tue d'un coup rapide et précis. «Sus à la fausse déesse!» fut la dernière chose qu'il entendit.",
         image: "./assets/images/perdu.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 735,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Comment aurait-il pu éviter ce destin?',
             destination: 'debut'
@@ -45,6 +51,8 @@ const chapters = {
         //[enlever image]
         video: "./assets/videos/rafale.mp4",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 859,
+        sonDuree: 2500,
         //sonSup: "./assets/sons/rozan_sho_ryu_ha.mp3",
         boutons: [{
             titre: "Continuer d'avancer dans le sanctuaire",
@@ -58,6 +66,8 @@ const chapters = {
         description: "Alors qu'il continue d'avancer dans le sanctuaire, se demandant comment il pourrait trouver le reste du «Nous» qu'a mentioné le chevalier qu'il a battu, au loin, Javier vois un autre chevalier de bronze fuire un chevalier d'or, le plus haut rang possible pour un chevalier, il est même plus fort la garde rapprochée d'Athéna.",
         image: "./assets/images/rencontre.jpeg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 1330,
+        sonDuree: 3000,
         boutons: [{
             titre: '«Que ce passe-t-il?»',
             destination: 'dilemme'
@@ -69,6 +79,8 @@ const chapters = {
         description: "Le chevalier de bronze supplie Javier de l'aider puisque seul, il ne peut rien contre un chevalier d'or, que fait Javier, devrait-il le croire?",
         image: "./assets/images/or.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 288,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Attaquer le chevalier de bronze',
             destination: 'attaquer'
@@ -90,6 +102,8 @@ const chapters = {
         description: "S'il y a une personne en qui on peut croire, c'est le chevalier d'or, le plus fort et le plus loyal des chevaliers! Javier donne un puissant coup de poing au crâne de l'autre chevalier de bronze, l'assomant instantanément. «Quel dommage, tu viens d'éliminer un des seuls aliés qui vous restait à vous, les loyaux» dit le chevalier d'or avans d'oblitérer le deux chevaliers de bronze devans lui",
         image: "./assets/images/perdu.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 735,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Comment aurait-il pu éviter ce destin?',
             destination: 'debut'
@@ -102,6 +116,8 @@ const chapters = {
         description: "Ne voulant pas prendre de décision hâtive, Javier attends un peu avant d'attaquer sans raison. Une décision qu'il regrette amèrement lorsqu'il voit le chavalier d'or réduire celui qui le fuiait en poussières avant de se tourner vers Javier, un sourire moqueur sur le visage.",
         image: "./assets/images/attendre.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 1743,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Oh non...',
             destination: 'riposte'
@@ -113,6 +129,8 @@ const chapters = {
         description: "«T'es un peu lent, toi.» Dis le chevalier d'or en s'aprochant. Javier n'as plus le temps d'hésiter le chevalier d'or n'est clairement pas de son côté mais que peut-il faire contre le plus puissant chevalier du sanctuaire?",
         image: "./assets/images/riposte.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 1749,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Un coup puissant, direct',
             destination: 'directe'
@@ -130,6 +148,8 @@ const chapters = {
         description: "Désespéré, Javier donne le plus de coups de poing possible, le plus vite possible mais aucun de ces petit coups rapides ne'est assez puissant pour battre un chevalier de bronze. Javier est oblitéré en un clin d'oeil",
         image: "./assets/images/perdu.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 735,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Comment aurait-il pu éviter ce destin?',
             destination: 'debut'
@@ -141,6 +161,9 @@ const chapters = {
         description: "Prennant avantage du fait que le chevalier d'or ne le prends clairement pas au sérieux, Javier se jette dans la gueule du loup à la vitesse de l'éclair et frappe le chevalier d'or dans le ventre de toutes ses forces. Le chevalier d'or est déstabilisé, ce qui laisse assez de temps à Javier pour porter un coup dévastateur à la gorge de son adversaire, le mettant hors d'état de nuire. «Tu as battu mon chevalier d'or à toi tous seul, impressionant...»",
         image: "./assets/images/directe.jpg",
         son: "./assets/sons/saint_seiya_ost_2.mp3",
+        sonStart: 569,
+        sonDuree: 3000,
+        //sonSup: "./assets/sons/rozan_sho_ryu_ha.mp3",
         boutons: [{
             titre: '«Qui va là?»',
             destination: 'meurtre'
@@ -153,6 +176,8 @@ const chapters = {
         description: "Ensemble, Javier et l'autre chevalier de bronze qui se présente sous le nom de Lancelot combattent le chevalier d'or et, ensemble, ils arrivent à défaire leur puissant ennemi. Lancelot explique ensuite la raison pour laquelle le chevalier d'or le chassait: il a découvert l'identité du traitre original et que le chevalier d'or avait rejoint sa cause. «Dis donc, tu est drôlement bien renseigné, toi...»",
         image: "./assets/images/aide.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 35,
+        sonDuree: 990,
         boutons: [{
             titre: '«Qui va là?»',
             destination: 'meurtre'
@@ -165,6 +190,9 @@ const chapters = {
         description: "Javier se retourne, ayant à peine le temps d'entrevoir la silouette du chef de la garde raprochée d'Athéna avant d'être anéanti par ce dernier.",
         image: "./assets/images/meurtre.jpeg",
         son: "./assets/sons/saint_seiya_ost_3.mp3",
+        sonStart: 1310,
+        sonDuree: 1800,
+        //sonSup: "./assets/sons/rozan_sho_ryu_ha.mp3",
         boutons: [{
             titre: '«Comment est-ce possible?»',
             destination: 'traitre'
@@ -176,6 +204,8 @@ const chapters = {
         description: "La garde raprochée d'Athéna, les hommes et femmes en qui Athéna à entièrement confience. Des anciens chevaliers qui sont suposés de n'avoir qu'une chose en commun: une loyauté sans faille envers la Déesse. Finalement, l'homme le plus proche de la déesse sera celui qui causera sa perte.",
         image: "./assets/images/traitre.jpg",
         son: "./assets/sons/saint_seiya_ost_3.mp3",
+        sonStart: 1767,
+        sonDuree: 2900,
         boutons: [{
             titre: "C'est fini...",
             destination: 'mort'
@@ -193,6 +223,8 @@ const chapters = {
         description: "Javier est mort, Athéna est maintenant à la merci de ceux qu'elle croyait être ses loyaux serviteurs.",
         image: "./assets/images/perdu.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 735,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Comment aurait-il pu éviter ce destin?',
             destination: 'debut'
@@ -206,6 +238,8 @@ const chapters = {
         //[enlever image]
         video: "./assets/videos/twist.mp4",
         son: "./assets/sons/saint_seiya_ost_3.mp3",
+        sonStart: 1092,
+        sonDuree: 3000,
         boutons: [{
             titre: '«Lancelot, vas-y!!!»',
             destination: 'fin'
@@ -217,6 +251,8 @@ const chapters = {
         description: "Sa conection à Athéna renforcée grâce à Javier, Lancelot arrive à surpasser la puissance du traître, un coup de poing titanèsque creuse un cratère dans le torse de ce dernier. L'ordre et la paix enfin revenus au sanctuaire, sachant qu'Athéna est saine et sauve, javier peut mourrir en paix, en véritable chevalier",
         image: "./assets/images/mort.jpg",
         son: "./assets/sons/saint_seiya_ost_1.mp3",
+        sonStart: 175,
+        sonDuree: 3000,
         boutons: [{
             titre: 'Recommencer?',
             destination: 'debut'
@@ -231,7 +267,7 @@ function goToChapter(chapter) {
         const titreRemplace = document.querySelector("#chapitre");
         const descriptionRemplace = document.querySelector("#para");
         const imageRemplace = document.querySelector("#image");
-        //const soundEffect = document.querySelector("#sunSup");
+        
 
         titreRemplace.textContent = chapters[chapter].titre;
         descriptionRemplace.textContent = chapters[chapter].description;
@@ -265,184 +301,28 @@ function goToChapter(chapter) {
 
         //code son:
         const sonRemplace = document.querySelector("#son");
-        sonRemplace.innerHTML = `<audio src="${chapters[chapter].son}" id="${chapter}-son"></audio>`;
+        const monAudio = document.createElement('audio');
+        monAudio.src = chapters[chapter].son;
+        sonRemplace.innerHTML = '';
+        sonRemplace.appendChild(monAudio);
 
-        if (chapter === "debut") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 494;
-                sonActuel.play();
+        //const soundEffect = document.querySelector("#sunSup");
+        //soundEffect.innerHTML = `<audio src="${chapters[chapter].sonSup}" id="${chapter}-sonSup"></audio>`;
 
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
 
-        if (chapter === "ambuscade" || chapter === "dilemme") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 288;
-                sonActuel.play();
 
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
+        monAudio.currentTime = chapters[chapter].sonStart;
+        monAudio.play();
+        setTimeout(function () {
+            monAudio.pause();
+        }, chapters[chapter].sonDuree);
 
-        if (chapter === "rafale") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            //let sonSup = document.querySelector()
-            setTimeout(function () {
-                sonActuel.currentTime = 859;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 2500);
-            });
-        }
-
-        if (chapter === "rencontre") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1330;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-
-        if (chapter === "aide") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 35;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 990);
-            });
-        }
-
-        if (chapter === "meurtre") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1310;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 1800);
-            });
-        }
-
-        if (chapter === "traitre") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1767;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 2900);
-            });
-        }
-
-        if (chapter === "twist") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1092;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-
-        if (chapter === "fin") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 175;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-
-        //sons du chemain alternatif
-        if (chapter === "attendre") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1743;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-
-        if (chapter === "riposte") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 1749;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-
-        if (chapter === "directe") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 569;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
-        //sons de mort
-        if (chapter === "bloque" || chapter === "attaquer" || chapter === "desespere" || chapter === "mort") {
-            let sonActuel = document.querySelector(`#${chapter}-son`);
-            setTimeout(function () {
-                sonActuel.currentTime = 735;
-                sonActuel.play();
-
-                setTimeout(function () {
-                    sonActuel.pause();
-                    sonActuel.currentTime = 0;
-                }, 3000);
-            });
-        }
 
         //si y'a de quoi à changer dans les sons:
         //ost 1: https://www.youtube.com/watch?v=47b3mWohtGY
         //ost 2: https://www.youtube.com/watch?v=yVb-yUlkszs
         //ost 3: https://www.youtube.com/watch?v=b_RRG2BuRn0&t=732s
-        //code son fini
+        
 
         const none = document.querySelector(".opt.big1");
 
@@ -497,7 +377,7 @@ function goToChapter(chapter) {
             killerQueen.play();
         })*/
 
-        
+
     }
 }
 
